698c96300d20fe72fae2f0b6635d2de13afe0641
HariHaran10112006 <hariharansh4@gmail.com>
2026-09-23 22:39:53 +0530

    Add AboutUs component
    Added AboutUs component with information about the nursery.

---
+++ src/AboutUs.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <section className="about-us">
+      <h2>About Paradise Nursery</h2>
+
+      <p>
+        Paradise Nursery is an online plant shop dedicated to bringing
+        beautiful and healthy houseplants into homes and workplaces.
+      </p>
+
+      <p>
+        We provide a variety of indoor plants including air-purifying,
+        aromatic, medicinal, and decorative plants.
+      </p>
+
+      <p>
+        Our goal is to make choosing and purchasing plants simple,
+        convenient, and enjoyable for every plant lover.
+      </p>
+    </section>
+  );
+}
+
+export default AboutUs;
